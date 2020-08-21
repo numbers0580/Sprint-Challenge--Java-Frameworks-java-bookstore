72f86604b71d4997da34acd12610900fc5aa8f45
numbers0580 <koreankid18@yahoo.com>
2020-08-21 14:18:21 -0600

    Fixed Environment Variables issue.

--- bookstore/src/main/java/com/lambdaschool/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/com/lambdaschool/bookstore/BookstoreApplication.java
@@ -46,14 +46,15 @@ public class BookstoreApplication
     public static void main(String[] args)
     {
         // Check to see if the environment variables exists. If they do not, stop execution of application.
+
         checkEnvironmentVariable("OAUTHCLIENTID");
         checkEnvironmentVariable("OAUTHCLIENTSECRET");
 
-        if (!stop)
-        {
+        if (!stop) {
             // so run the application!
-            SpringApplication.run(BookstoreApplication.class,
-                                  args);
+            SpringApplication.run(BookstoreApplication.class, args);
         }
+
+        //SpringApplication.run(BookstoreApplication.class, args);
     }
 }
