2abb662e29c44686678f55d49714b8a0c3656661
numbers0580 <koreankid18@yahoo.com>
2020-08-21 11:50:33 -0600

    Resolved tests by mocking data. Issues with deployment now.

--- bookstore/src/test/java/com/lambdaschool/bookstore/controllers/BookControllerTest.java
+++ bookstore/src/test/java/com/lambdaschool/bookstore/controllers/BookControllerTest.java
@@ -2,7 +2,10 @@ package com.lambdaschool.bookstore.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lambdaschool.bookstore.BookstoreApplication;
+import com.lambdaschool.bookstore.models.Author;
 import com.lambdaschool.bookstore.models.Book;
+import com.lambdaschool.bookstore.models.Section;
+import com.lambdaschool.bookstore.models.Wrote;
 import com.lambdaschool.bookstore.services.BookService;
 import org.junit.After;
 import org.junit.Before;
@@ -24,7 +27,9 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 
@@ -70,6 +75,68 @@ public class BookControllerTest
          * Note that since we are only testing bookstore data, you only need to mock up bookstore data.
          * You do NOT need to mock up user data. You can. It is not wrong, just extra work.
          */
+
+        Author a1 = new Author("John", "Mitchell");
+        Author a2 = new Author("Dan", "Brown");
+        Author a3 = new Author("Jerry", "Poe");
+        Author a4 = new Author("Wells", "Teague");
+        Author a5 = new Author("George", "Gallinger");
+        Author a6 = new Author("Ian", "Stewart");
+
+        a1.setAuthorid(1);
+        a2.setAuthorid(2);
+        a3.setAuthorid(3);
+        a4.setAuthorid(4);
+        a5.setAuthorid(5);
+        a6.setAuthorid(6);
+
+        Section s1 = new Section("Fiction");
+        Section s2 = new Section("Technology");
+        Section s3 = new Section("Travel");
+        Section s4 = new Section("Business");
+        Section s5 = new Section("Religion");
+
+        s1.setSectionid(1);
+        s2.setSectionid(2);
+        s3.setSectionid(3);
+        s4.setSectionid(4);
+        s5.setSectionid(5);
+
+        Set<Wrote> wrote = new HashSet<>();
+        wrote.add(new Wrote(a6, new Book()));
+        Book b1 = new Book("Flatterland", "9780738206752", 2001, s1);
+        b1.setWrotes(wrote);
+        b1.setBookid(1);
+        bookList.add(b1);
+
+        wrote = new HashSet<>();
+        wrote.add(new Wrote(a2, new Book()));
+        Book b2 = new Book("Digital Fortess", "9788489367012", 2007, s1);
+        b2.setWrotes(wrote);
+        b2.setBookid(2);
+        bookList.add(b2);
+
+        wrote = new HashSet<>();
+        wrote.add(new Wrote(a2, new Book()));
+        Book b3 = new Book("The Da Vinci Code", "9780307474278", 2009, s1);
+        b3.setWrotes(wrote);
+        b3.setBookid(3);
+        bookList.add(b3);
+
+        wrote = new HashSet<>();
+        wrote.add(new Wrote(a5, new Book()));
+        wrote.add(new Wrote(a3, new Book()));
+        Book b4 = new Book("Essentials of Finance", "1314241651234", 0, s4);
+        b4.setWrotes(wrote);
+        b4.setBookid(4);
+        bookList.add(b4);
+
+        wrote = new HashSet<>();
+        wrote.add(new Wrote(a4, new Book()));
+        Book b5 = new Book("Calling Texas Home", "1885171382134", 2000, s3);
+        b5.setWrotes(wrote);
+        b5.setBookid(5);
+        bookList.add(b5);
     }
 
     @After
@@ -110,6 +177,16 @@ public class BookControllerTest
 
     @Test
     public void getNoBookById() throws Exception {
+        String apiUrl = "/books/book/1";
+        Mockito.when(bookService.findBookById(187)).thenReturn(null);
+
+        RequestBuilder rb = MockMvcRequestBuilders.get(apiUrl).accept(MediaType.APPLICATION_JSON);
+        MvcResult r = mockMvc.perform(rb).andReturn();
+        String tr = r.getResponse().getContentAsString();
+
+        String er = "";
+
+        assertEquals(er, tr);
     }
 
     @Test
